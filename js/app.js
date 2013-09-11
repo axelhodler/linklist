c1813831f4c2f57896f448064ddeecd97cca1f52
xorrr <derxor@gmail.com>
2013-09-11 22:41:04 +0200

    redirect to links route when visiting the index

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ App = Ember.Application.create();
 App.Router.map(function() {
   this.resource('links');
 });
+
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('links');
+  }
+});
